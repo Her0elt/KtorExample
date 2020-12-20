3bf23e633360931b78c9ea06bc5bb84588d8c6f4
Hermann <hermann.owren.elton@hotmail.com>
2020-12-20 20:28:27 +0100

    github workflow

--- src/Application.kt
+++ src/Application.kt
@@ -18,7 +18,7 @@ import io.ktor.routing.Routing
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
+//@kotlin.jvm.JvmOverloads
 fun Application.module() {
     HttpClient(Apache) {
         install(ContentNegotiation) {
